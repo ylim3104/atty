588e97bdd49b9f70423912a6036c4d087118cfdb
Radman <97783226+radmanon@users.noreply.github.com>
2026-03-15 09:57:18 -0700

    resolve conflict

--- src/atty_linters/rules/rules.ts
+++ src/atty_linters/rules/rules.ts
@@ -5,7 +5,7 @@ import { CompareNone } from './compareNone';
 import { MissingArgument } from "./missingArgument";
 import { BreakOutsideLoop } from "./breakOutsideLoop";
 import { ReturnOutsideFunction } from "./returnOutsideFunction";
-import { UnclosedBracketOrString } from "./unclosedBracketOrString"; 
+import { UnclosedBracketOrString } from "./unclosedBracketOrString";
 import { ImportNotFound } from "./importNotFound";
 import { ZeroDivisionErrorRule } from "./zeroDivisionError";
 import { BareExcept } from './bareExcept';
@@ -23,14 +23,14 @@ import { IndentationError } from './indentationError';
 
 export class Rules {
     language: string;
-    ruleList: BaseRule [];
-    
+    ruleList: BaseRule[];
+
     constructor(language: string) {
         this.language = language;
         this.ruleList = [new CompareNone(language), new SyntaxMissingColon(language), new MissingArgument(language), new BreakOutsideLoop(language), new ReturnOutsideFunction(language), new UnclosedBracketOrString(language), new ImportNotFound(language), new ZeroDivisionErrorRule(language), new BareExcept(language), new UnusedImport(language), new UnreachableCode(language), new RedefinedBuiltin(language), new MutableDefaultArg(language), new UseEnumerate(language), new FileNotClosed(language), new UnusedVariable(language), new UseDictComprehension(language)];
     }
 
     public run(diagnostics: vscode.Diagnostic[], node: Parser.SyntaxNode) {
-        this.ruleList.forEach((rule) => { rule.walk(diagnostics, node)})
+        this.ruleList.forEach((rule) => { rule.walk(diagnostics, node) })
     }
 }
